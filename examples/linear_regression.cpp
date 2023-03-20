13b059d051734530ce4a6149c0d2feebd3e9d33c
oluseyivictor <oloniyov@gmail.com>
2023-03-20 16:33:58 +0000

    Format structure to align on Github

--- examples/linear_regression.cpp
+++ examples/linear_regression.cpp
@@ -24,23 +24,24 @@ int main(){
 		}
 	}
 
-	std::normal_distribution<> normal_noise(0,0.1);
-	for(std::size_t i = 0; i != num_data_points; ++i){
-		y[i] = sum(convert_row(X,i)) + normal_noise(gen) + 1;
+	std::normal_distribution<> normal_noise(0, 0.1);
+	for (std::size_t i = 0; i != num_data_points; ++i)
+	{
+		y[i] = sum(convert_row(X, i)) + normal_noise(gen) + 1;
 	}
 
-    /* Linear Model*/
-    LinearRegression model(X, y);
-    model.train(5000);
+	/* Linear Model*/
+	LinearRegression model(X, y);
+	model.train(5000);
 
-    auto input = convert_row(X, 1);
-    auto predicted = model.predict(input);
+	auto input = convert_row(X, 1);
+	auto predicted = model.predict(input);
 	double actual = y[1];
 
-    std::cout<<actual<<std::endl;
-	std::cout<<predicted<<std::endl;
+	std::cout << actual << std::endl;
+	std::cout << predicted << std::endl;
 
-	std::cout<< "error = "<<((actual-predicted[0]) / actual)<<std::endl;
+	std::cout << "error = " << ((actual - predicted[0]) / actual) << std::endl;
 
-    return 1;
+	return 1;
 }
