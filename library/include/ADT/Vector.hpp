26157a48c280b58a97ce7c2aca877086601c2c4b
oluseyivictor <oloniyov@gmail.com>
2023-03-31 12:09:00 +0100

    Added Parallel calculation to vector(Refactor)

--- library/include/ADT/Vector.hpp
+++ library/include/ADT/Vector.hpp
@@ -6,9 +6,30 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
+#include <boost/thread.hpp>
 #include "Matrix.hpp"
 
 namespace phoenix {
+
+    template <typename T>
+void matrix_vector_multiply_thread(const Matrix<T>& m2, const Vector<T>& v1,
+                                   Vector<T>& result, std::size_t row_start, std::size_t row_end);
+
+template <typename T>
+void matrix_vector_multiply(const Matrix<T>& m2, const Vector<T>& v1, Vector<T>& result, std::size_t num_threads);
+
+template <typename T>
+void matrix_vector_multiply(const Matrix<T>& m2, const Vector<T>& v1, Vector<T>& result);
+
+template <typename T>
+void vector_vector_add(const Vector<T>& v1, const Vector<T>& v2, Vector<T>& result);
+
+
+template <typename T>
+void vector_vector_add(const Vector<T>& m2, const Vector<T>& v1, Vector<T>& result, int num_threads);
+
+template <typename T>
+void vector_addition_thread (const Vector<T>& v1, const Vector<T>& v2, Vector<T>& result, int start, int end);
 /**
  *  @brief The Vector class represents a 1D matrix with generic programming.
  *
@@ -66,6 +87,8 @@ public:
         }
         return Matrix<T>::operator()(i, 0);
     }
+    T& operator()(std::size_t i) { return Matrix<T>::operator()(i, 0); }
+    const T& operator()(std::size_t i) const { return Matrix<T>::operator()(i, 0); }
 
     /**
      * @brief Multiplies the vector by a scalar value.
@@ -91,22 +114,20 @@ public:
      * @return Vector<T> The resulting vector after adding the two input vectors.
      * @throws std::invalid_argument if the number of rows of the two vectors are not equal.
      */
-    friend Vector<T> operator+(const Vector &v, const Vector &other)
+   friend Vector<T> operator+(const Vector<T> &v, const Vector<T> &other)
+{
+    if (v.getRows() != other.getRows())
     {
-        if (v.getRows() != other.getRows())
-        {
-            throw std::invalid_argument("The number of vector rows must be equal in vector + error. ");
-        }
-
-        Vector<T> result(other.size());
-
-        for (int i = 0; i < v.size(); i++)
-        {
-            result[i] = v[i] + other[i];
-        }
-
-        return result;
+        throw std::invalid_argument("The number of vector rows must be equal in vector + error. ");
     }
+
+    Vector<T> result(other.size());
+
+    vector_vector_add(v, other, result, boost::thread::hardware_concurrency());
+
+    
+    return result;
+}
 
     /**
      * @brief Multiplies the vector by a scalar value.
@@ -148,24 +169,21 @@ public:
      * @return A new Vector object representing the result of the multiplication.
      * @throws std::invalid_argument if the number vector rows and the matrix column vectors are not equal.
      */
-    friend Vector<T> operator*(Matrix<T> &m2, const Vector<T> &v1)
+
+   friend Vector<T> operator*(Matrix<T> &m2, const Vector<T> &v1)
     {
         if (v1.getRows() != m2.getCols())
         {
             throw std::invalid_argument("The number of vector rows must be equal to Matrix column. ");
         }
-        Vector<double> result(m2.getRows());
-        for (int k = 0; k < m2.getRows(); k++)
-        {
-            result[k] = 0;
-            for (int i = 0; i < v1.getRows(); i++)
-            {
 
-                result[k] += v1[i] * m2[k][i];
-            }
-        }
+        Vector<double> result(m2.getRows());
+
+        matrix_vector_multiply(m2, v1, result, boost::thread::hardware_concurrency());
+
         return result;
     }
+
     
     /**
      * @brief Assigns a vector of elements to the current vector using an initializer list.
@@ -185,6 +203,108 @@ public:
     }
 };
 
+
+
+
+template <typename T>
+void matrix_vector_multiply_thread(const Matrix<T>& m2, const Vector<T>& v1,
+                                   Vector<T>& result, std::size_t row_start, std::size_t row_end)
+{
+    for (std::size_t k = row_start; k < row_end; ++k) {
+        T sum = 0;
+        for (std::size_t i = 0; i < v1.getRows(); ++i) {
+            sum += v1[i] * m2(k,i);
+        }
+        result[k] = sum;
+    }
 }
 
+// Define a function to launch multiple threads for matrix-vector multiplication
+template <typename T>
+void matrix_vector_multiply(const Matrix<T>& m2, const Vector<T>& v1, Vector<T>& result, std::size_t num_threads)
+{
+
+    std::vector<boost::thread> threads(num_threads);
+
+    std::size_t rows_per_thread = m2.getRows() / num_threads;
+
+    for (std::size_t i = 0; i < num_threads; ++i) {
+        std::size_t row_start = i * rows_per_thread;
+        std::size_t row_end = (i == num_threads - 1) ? m2.getRows() : (i + 1) * rows_per_thread;
+        threads[i] = boost::thread(matrix_vector_multiply_thread<T>, std::cref(m2), std::cref(v1),
+                                        std::ref(result), row_start, row_end);
+}
+
+for (auto& thread : threads) {
+    thread.join();
+}
+}
+
+template <typename T>
+void matrix_vector_multiply(const Matrix<T>& m2, const Vector<T>& v1, Vector<T>& result)
+{
+    for (int k = 0; k < m2.getRows(); k++)
+    {
+        result[k] = 0;
+        for (int i = 0; i < v1.getRows(); i++)
+        {
+
+            result[k] += v1[i] * m2[k][i];
+        }
+    }
+}
+
+template <typename T>
+void vector_addition_thread (const Vector<T>& v1, const Vector<T>& v2, Vector<T>& result, int start, int end) {
+        for (int i = start; i < end; i++) {
+            result[i] = v1[i] + v2[i];
+        }
+}
+
+template <typename T>
+void vector_vector_add(const Vector<T> &v, const Vector<T> &other, Vector<T>& result, int num_threads)
+{
+
+    // Define the thread function for vector addition
+    auto vector_addition_thread = [](const Vector<T>& v1, const Vector<T>& v2, Vector<T>& result, int start, int end) {
+        for (int i = start; i < end; i++) {
+            result[i] = v1[i] + v2[i];
+        }
+    };
+
+    // Launch threads to perform vector addition in parallel
+    std::vector<boost::thread> threads;
+    int chunk_size = v.size() / num_threads;
+    int remainder = v.size() % num_threads;
+    int start = 0;
+    int end = 0;
+
+    for (int i = 0; i < num_threads; i++) {
+        start = end;
+        end += chunk_size;
+        if (i == num_threads - 1) {
+            end += remainder;
+        }
+        threads.emplace_back(vector_addition_thread, std::cref(v), std::cref(other),
+                              std::ref(result), start, end);
+    }
+
+    // Wait for all threads to finish
+    for (auto& thread : threads) {
+        thread.join();
+    }
+
+}
+
+template <typename T>
+void vector_vector_add(const Vector<T>& v1, const Vector<T>& v2, Vector<T>& result)
+{ 
+    for (int i = 0; i < v1.size(); i++)
+        {
+            result[i] = v1[i] + v2[i];
+        }
+}
+
+
+}
 #endif
