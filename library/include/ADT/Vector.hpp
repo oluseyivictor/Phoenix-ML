23a152cab9e508230691798c64d8708998df6721
oluseyivictor <oloniyov@gmail.com>
2023-04-04 12:24:07 +0100

    Refactored parallel function to new function

--- library/include/ADT/Vector.hpp
+++ library/include/ADT/Vector.hpp
@@ -6,32 +6,12 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
-#include <boost/thread.hpp>
 #include <config.hpp>
 #include "Matrix.hpp"
+#include "parallel.h"
 
 
 namespace phoenix {
-
-    template <typename T>
-void matrix_vector_multiply_thread(const Matrix<T>& m2, const Vector<T>& v1,
-                                   Vector<T>& result, std::size_t row_start, std::size_t row_end);
-
-template <typename T>
-void matrix_vector_multiply(const Matrix<T>& m2, const Vector<T>& v1, Vector<T>& result, std::size_t num_threads);
-
-template <typename T>
-void matrix_vector_multiply(const Matrix<T>& m2, const Vector<T>& v1, Vector<T>& result);
-
-template <typename T>
-void vector_vector_add(const Vector<T>& v1, const Vector<T>& v2, Vector<T>& result);
-
-
-template <typename T>
-void vector_vector_add(const Vector<T>& m2, const Vector<T>& v1, Vector<T>& result, int num_threads);
-
-template <typename T>
-void vector_addition_thread (const Vector<T>& v1, const Vector<T>& v2, Vector<T>& result, int start, int end);
 /**
  *  @brief The Vector class represents a 1D matrix with generic programming.
  *
@@ -89,7 +69,25 @@ public:
         }
         return Matrix<T>::operator()(i, 0);
     }
+
+    /**
+     * @brief Overloaded opening braces operator to accesses vector element at index i.
+     *
+     * @param i row Row index of the element.
+     *
+     * @return T& Reference to the element at the specified index.
+     * @throws std::invalid_argument if wrong index is accessed
+     */
     T& operator()(std::size_t i) { return Matrix<T>::operator()(i, 0); }
+
+    /**
+     * @brief Overloaded opening braces operator to accesses vector element at index i.
+     *
+     * @param i row Row index of the element.
+     *
+     * @return const T& Reference to the element at the specified index.
+     * @throws std::invalid_argument if wrong index is accessed
+     */
     const T& operator()(std::size_t i) const { return Matrix<T>::operator()(i, 0); }
 
     /**
@@ -210,109 +208,6 @@ public:
         }
     }
 };
-
-
-
-
-template <typename T>
-void matrix_vector_multiply_thread(const Matrix<T>& m2, const Vector<T>& v1,
-                                   Vector<T>& result, std::size_t row_start, std::size_t row_end)
-{
-    for (std::size_t k = row_start; k < row_end; ++k) {
-        T sum = 0;
-        for (std::size_t i = 0; i < v1.getRows(); ++i) {
-            sum += v1[i] * m2(k,i);
-        }
-        result[k] = sum;
-    }
-}
-
-// Define a function to launch multiple threads for matrix-vector multiplication
-template <typename T>
-void matrix_vector_multiply(const Matrix<T>& m2, const Vector<T>& v1, Vector<T>& result, std::size_t num_threads)
-{
-
-    std::vector<boost::thread> threads(num_threads);
-
-    std::size_t rows_per_thread = m2.getRows() / num_threads;
-
-    for (std::size_t i = 0; i < num_threads; ++i) {
-        std::size_t row_start = i * rows_per_thread;
-        std::size_t row_end = (i == num_threads - 1) ? m2.getRows() : (i + 1) * rows_per_thread;
-        threads[i] = boost::thread(matrix_vector_multiply_thread<T>, std::cref(m2), std::cref(v1),
-                                        std::ref(result), row_start, row_end);
-}
-
-for (auto& thread : threads) {
-    thread.join();
-}
-}
-
-template <typename T>
-void matrix_vector_multiply(const Matrix<T>& m2, const Vector<T>& v1, Vector<T>& result)
-{
-    for (int k = 0; k < m2.getRows(); k++)
-    {
-        result[k] = 0;
-        for (int i = 0; i < v1.getRows(); i++)
-        {
-
-            result[k] += v1[i] * m2[k][i];
-        }
-    }
-}
-
-template <typename T>
-void vector_addition_thread (const Vector<T>& v1, const Vector<T>& v2, Vector<T>& result, int start, int end) {
-        for (int i = start; i < end; i++) {
-            result[i] = v1[i] + v2[i];
-        }
-}
-
-template <typename T>
-void vector_vector_add(const Vector<T> &v, const Vector<T> &other, Vector<T>& result, int num_threads)
-{
-
-    // Define the thread function for vector addition
-    auto vector_addition_thread = [](const Vector<T>& v1, const Vector<T>& v2, Vector<T>& result, int start, int end) {
-        for (int i = start; i < end; i++) {
-            result[i] = v1[i] + v2[i];
-        }
-    };
-
-    // Launch threads to perform vector addition in parallel
-    std::vector<boost::thread> threads;
-    int chunk_size = v.size() / num_threads;
-    int remainder = v.size() % num_threads;
-    int start = 0;
-    int end = 0;
-
-    for (int i = 0; i < num_threads; i++) {
-        start = end;
-        end += chunk_size;
-        if (i == num_threads - 1) {
-            end += remainder;
-        }
-        threads.emplace_back(vector_addition_thread, std::cref(v), std::cref(other),
-                              std::ref(result), start, end);
-    }
-
-    // Wait for all threads to finish
-    for (auto& thread : threads) {
-        thread.join();
-    }
-
-}
-
-template <typename T>
-void vector_vector_add(const Vector<T>& v1, const Vector<T>& v2, Vector<T>& result)
-{ 
-    for (int i = 0; i < v1.size(); i++)
-        {
-            result[i] = v1[i] + v2[i];
-        }
-}
-
 
 }
 #endif
