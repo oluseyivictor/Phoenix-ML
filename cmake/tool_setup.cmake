ec8dfbd2be191cc5c9f56c6c112b663ae3562b97
Victor Oloniyo <victor.oloniyo@mkopa.com>
2024-09-17 15:30:18 +0100

    Main branch broken cos of dll shared error

--- cmake/tool_setup.cmake
+++ cmake/tool_setup.cmake
@@ -1,9 +1,27 @@
 include(FetchContent)
 
 find_package(Python 3.8 REQUIRED COMPONENTS Interpreter Development)
+find_package(PythonInterp 3 REQUIRED)
+find_package(PythonLibs 3 REQUIRED)
+
 # Print the Python 3.8 include directory
 # Add the found include directory to the include paths
 include_directories(${_Python_INCLUDE_DIR})
+
+execute_process(
+    COMMAND ${_Python_EXECUTABLE} -c "import numpy"
+    RESULT_VARIABLE numpy_check_result
+    OUTPUT_QUIET ERROR_QUIET
+    )
+
+if(NOT numpy_check_result EQUAL 0)
+    message(WARNING "Python 'numpy' module is not available! Installing it...")
+    execute_process(
+        COMMAND ${_Python_EXECUTABLE} -m pip install numpy==1.26.0
+        RESULT_VARIABLE pip_install_result
+        OUTPUT_QUIET ERROR_QUIET
+    )
+endif()
 
 find_path(DIR_PATH
     NAMES boost
@@ -26,6 +44,8 @@ if(NOT DIR_PATH)
     set(Boost_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/tools/boost_1_86_0 CACHE PATH "Boost Root File")
     set(Boost_LIBRARY_DIRS ${CMAKE_SOURCE_DIR}/tools/boost_1_86_0/stage/lib CACHE PATH "Boost Root File")
 
+      # Check if the third-party 'numpy' module is available
+    
     
     # change this to use bootstrap.sh for linux
     execute_process(
@@ -44,7 +64,7 @@ if(NOT DIR_PATH)
         )
 
         execute_process(
-            COMMAND b2 stage --with-thread --with-system --with-python3 --with-numpy3
+            COMMAND b2 stage --with-thread --with-system --with-python address-model=64 --debug-configuration
             WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/tools/boost_1_86_0
         )
     endif()
@@ -61,9 +81,19 @@ set(Boost_library
     ${Boost_LIBRARY_DIRS}/libboost_thread-mgw13-mt-x64-1_86.a
     ${Boost_LIBRARY_DIRS}/libboost_system-mgw13-mt-x64-1_86.a
     ${Boost_LIBRARY_DIRS}/libboost_chrono-mgw13-mt-x64-1_86.a
-    ${Boost_LIBRARY_DIRS}/libboost_python311-mgw13-mt-x64-1_86.a
+   # ${Boost_LIBRARY_DIRS}/libboost_python312-mgw13-mt-x64-1_86.a
+   # ${Boost_LIBRARY_DIRS}/libboost_numpy312-mgw13-mt-x64-1_86.a
     pthread
     )
 
 message(STATUS "Boost include directory: ${Boost_INCLUDE_DIRS}")
 message(STATUS "Boost library directory: ${Boost_LIBRARY_DIRS}")
+
+FetchContent_Declare(
+    googletest
+    GIT_REPOSITORY https://github.com/google/googletest.git
+    GIT_TAG v1.13.0
+    GIT_SHALLOW TRUE
+)
+
+FetchContent_MakeAvailable(googletest)
